196324486ba5bac019dde6641d80a41a257841e3
MR. OPPY <86025912+MrOppy@users.noreply.github.com>
2024-11-18 06:01:40 +0600

    Add files via upload

---
+++ js/inline_scripts.js
@@ -0,0 +1,3 @@
+WebFont.load({  google: {    families: ["DM Sans:regular,600"]  }});
+
+!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart"in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);
